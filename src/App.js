85bead814756de244c3a347731bc6b18d408fc91
priyanka-chauhan <prchauhan0704@gmail.com>
2024-07-22 22:25:48 +0530

    initial react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>design components</h1>
+        <Button />
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
